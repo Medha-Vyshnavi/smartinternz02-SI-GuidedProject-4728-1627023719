b1ecb84c5c5edb2eccea290ca7a6b1d9f599cc0d
Medha-Vyshnavi <88196010+Medha-Vyshnavi@users.noreply.github.com>
2021-08-01 21:48:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,52 @@
-import numpy as np
-import pandas as pd
 import os
+import pandas as pd
+import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing  import image
 import tensorflow as tf
-
 from flask import Flask , request, render_template
-from werkzeug.utils import secure_filename
-from tensorflow.python.keras.backend import set_session
+from gevent.pywsgi import WSGIServer
 
-app=Flask(__name__)
-global sess
-sess = tf.Session()
-global graph
-graph=tf.compat.v1.get_default_graph()
-set_session(sess)
+app=Flask(_name_)
 
-model = load_model("vegetable.h5")
-model1=load_model("fruitnames.h5")
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def Home():
     return render_template("Home.html")
 
-@app.route('/Predict')
+@app.route('/predict')
 def GetStarted():
     return render_template('Predict.html')
+@app.route('/')
+def index():
+    return render_template('Predict.html')
 
-@app.route('/Predict',methods=['GET','POST'])		
-def Predict():
-    if request.method == 'POST':
-        
-        a = request.files['image']
-
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', secure_filename(a.filename))
-        a.save(file_path)
-        img = image.load_img(file_path, target_size=(128, 128))
-        
-        b = image.img_to_array(img)
-        b = np.expand_dims(b, axis=0)
-        preds = model.predict(b)
-        index = ["Pepper_bell___Bacterial_spot","Pepper_bell___healthy","Potato___Early_blight","Potato___Late_blight","Potato___healthy","Tomato___Bacterial_spot","Tomato___Late_blight","Tomato___Leaf_Mold","Tomato___Septoria_leaf_spot"]
-        index1 =["Apple___Black_rot","Apple___healthy","Corn_(maize)___Northern_Leaf_Blight","Corn_(maize)___healthy","Peach___Bacterial_spot","Peach___healthy"]
-        plant=request.form['plant']
-        print(plant)
-        if(plant=="vegetable"):
-            with graph.as_default():
-                set_session(sess)
-                preds = model.predict_classes(b)
-            print(preds)
-            df=pd.read_excel('precautions - veg.xlsx')
-            print(df.iloc[preds[0]]['caution'])
-        else:
-            with graph.as_default():
-                set_session(sess)
-                preds = model1.predict_classes(b)
-                
-            df=pd.read_excel('precautions - fruits.xlsx')
-            print(df.iloc[preds[0]]['caution'])
-            result=str(index[preds[0]])
-        return result
+@app.route('/predict',methods=['POST'])		
+def predict():
+    model_name=""
+    excel_name=""
+    plant=request.form['plant']
+    if(plant=="vegetable"):
+        model_name=r"E:\VIT\PlantFertilizerRecommendation\Flaskweb\vegetable.h5"
+        excel_name='precautions - vegetables.xlsx'
+    elif(plant=="fruit"):
+        model_name=r"E:\VIT\PlantFertilizerRecommendation\Flaskweb\fruitnames.h5"
+        excel_name='precautions - fruits.xlsx'
+    a = request.files['image']
+    basepath = os.path.dirname(_file_)
+    filepath = os.path.join(basepath,'uploads',a.filename)
+    a.save(filepath)
+    img = image.load_img(filepath,target_size = (64,64))
+    model = load_model (model_name)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x,axis = 0)
+    p=model.predict(x)
+    pred = np.argmax(model.predict(x))
+    df=pd.read_excel(excel_name)
+    return render_template("Predict.html", result=df.iloc[pred.item(),0])
     
 @app.route('/Logout')
 def Logout():
     return render_template('Logout.html')
 
-if __name__ ==  "__main__":
+if _name_ ==  "_main_":
      app.run(debug=True)
-
